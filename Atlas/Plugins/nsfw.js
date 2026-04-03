82dd50658aca99e932cf45a02735ab3e008409e9
Marin Auto-Saver <marin-bot@auto.sync>
2026-04-03 19:32:35 +0530

    💾 Auto-saved Multi-Bot Empire [skip ci]

--- Atlas/Plugins/nsfw.js
+++ Atlas/Plugins/nsfw.js
@@ -1,25 +1,108 @@
-import { setNSFW, delNSFW } from "../System/MongoDB/MongoDb_Core.js";
+import axios from "axios";
+import { checkNSFW } from "../System/MongoDB/MongoDb_Core.js";
+
+const NSFW_COMMANDS = {
+  animal: "animal",
+  animalears: "animalears",
+  anusview: "anusview",
+  ass: "ass",
+  barefoot: "barefoot",
+  bed: "bed",
+  bell: "bell",
+  bikini: "bikini",
+  blonde: "blonde",
+  bondage: "bondage",
+  bra: "bra",
+  breasthold: "breasthold",
+  breasts: "breasts",
+  bunnygirl: "bunnygirl",
+  bunnyears: "bunnyears",
+  chain: "chain",
+  closeview: "closeview",
+  cloudsview: "cloudseview",
+  cum: "cum",
+  dress: "dress",
+  elbowgloves: "elbowgloves",
+  erectnipples: "erectnipples",
+  fateseries: "fateserie",
+  fingering: "fingering",
+  flatchest: "flatchest",
+  food: "food",
+  nsfwfoxgirl: "nsfwfoxgirl",
+  gamecg: "gamecg",
+  genshin: "genshin",
+  glasses: "glasses",
+  gloves: "gloves",
+  greenhair: "greenhair",
+  hatsunemiku: "hatsunemiku",
+  hcatgirl: "hcatgirl",
+  headband: "headband",
+  headdress: "headdress",
+  headphones: "headphones",
+  hloli: "hloli",
+  hneko: "hnek",
+  hololove: "hololove",
+  horns: "horns",
+  inshorts: "inshorts",
+  japanesecloths: "japanesecloths",
+  necklace: "necklace",
+  nipples: "nipples",
+  nobra: "nobra",
+  hbeach: "hbeach",
+  hbell: "hbell",
+  hdemon: "hdemon",
+  hidol: "hidol",
+  hmaid: "hmaid",
+  hvampire: "hvampire",
+  nude: "hvmpire",
+  origial: "original",
+  sex: "sex",
+};
 
 export default {
   name: "nsfw",
-  alias: [],
-  description: "Enable/Disable NSFW",
+  alias: Object.keys(NSFW_COMMANDS),
+  description: "High Qualit NSFW Images",
 
-  start: async (Atlas, m, { text, isAdmin, isBotAdmin }) => {
-    if (!m.isGroup) return m.reply("Group only ❌");
-    if (!isAdmin) return m.reply("Admin only ❌");
-    if (!isBotAdmin) return m.reply("Bot must be admin ❌");
+  start: async (Atlas, m, { prefix, inputCMD }) => {
+    if (m.isGroup) {
+      const nsfw = await checkNSFW(m.from);
 
-    if (!text) return m.reply("Use: nsfw on/off");
-
-    if (text === "on") {
-      await setNSFW(m.from);
-      return m.reply("NSFW Enabled 🔥");
+      if (!nsfw) {
+        return m.reply(
+          `NSFW disabled ❌\n\nEnable using:\n*${prefix}nsfw on*`
+        );
+      }
     }
 
-    if (text === "off") {
-      await delNSFW(m.from);
-      return m.reply("NSFW Disabled ❌");
+    try {
+      // command check
+      const endpoint = NSFW_COMMANDS[inputCMD];
+
+      if (!endpoint) {
+        return m.reply("Invalid NSFW command ❌");
+      }
+
+      // loading
+      await m.reply("Fetching... 🔞");
+
+      // api call
+      const res = await axios.get(
+        `https://stenx-apis.vercel.app/api/nsfw/${endpoint}`
+      );
+      const img = res.data.url;
+      await Atlas.sendMessage(
+        m.from,
+        {
+          image: { url: img },
+          caption: `🔞 NSFW: ${endpoint}`
+        },
+        { quoted: m }
+      );
+
+    } catch (e) {
+      console.log(e);
+      m.reply("Error fetching NSFW ❌");
     }
   },
 };
